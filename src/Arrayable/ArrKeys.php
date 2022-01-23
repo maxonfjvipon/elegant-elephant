846e1975b8abd4fb6b349b55d1ef137dfd46159e
maximtrunnikov <m.trunnikov@mail.ru>
2022-01-23 21:52:16 +0300

    Added ArrKeys + tests

---
+++ src/Arrayable/ArrKeys.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Maxonfjvipon\Elegant_Elephant\Arrayable;
+
+use Exception;
+use Maxonfjvipon\Elegant_Elephant\Arrayable;
+
+/**
+ * Array keys.
+ * @package Maxonfjvipon\Elegant_Elephant\Arrayable
+ */
+class ArrKeys implements Arrayable
+{
+
+    /**
+     * @var Arrayable $arrayable
+     */
+    private Arrayable $arrayable;
+
+    /**
+     * Ctor wrap.
+     * @param array $array
+     * @return ArrKeys
+     */
+    public static function ofArray(array $array): ArrKeys
+    {
+        return ArrKeys::ofArrayble(ArrayableOf::array($array));
+    }
+
+    /**
+     * Ctor wrap.
+     * @param Arrayable $arrayable
+     * @return ArrKeys
+     */
+    public static function ofArrayble(Arrayable $arrayable): ArrKeys
+    {
+        return new self($arrayable);
+    }
+
+    /**
+     * Ctor.
+     * @param Arrayable $arrayable
+     */
+    private function __construct(Arrayable $arrayable)
+    {
+        $this->arrayable = $arrayable;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function asArray(): array
+    {
+        return array_keys($this->arrayable->asArray());
+    }
+}
---
+++ tests/Arrayable/ArrKeysTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Maxonfjvipon\Elegant_Elephant\Tests\Arrayable;
+
+use Exception;
+use Maxonfjvipon\Elegant_Elephant\Arrayable\ArrayableOf;
+use Maxonfjvipon\Elegant_Elephant\Arrayable\ArrKeys;
+use Maxonfjvipon\Elegant_Elephant\Arrayable\ArrValues;
+
+class ArrKeysTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @throws Exception
+     */
+    public function testAsArray(): void
+    {
+        $expected = [1, 2, 3, 4];
+        $arr = [
+            1 => "value1",
+            2 => "value2",
+            3 => "value3",
+            4 => "value4"
+        ];
+
+        $this->assertEquals(
+            $expected,
+            ArrKeys::ofArray($arr)->asArray()
+        );
+        $this->assertEquals(
+            $expected,
+            ArrKeys::ofArrayble(ArrayableOf::array($arr))->asArray()
+        );
+        $this->assertNotEquals(
+            $expected,
+            ArrKeys::ofArray([
+                0 => 1,
+                1 => 2,
+                2 => 3,
+                3 => 4
+            ])->asArray()
+        );
+    }
+}
