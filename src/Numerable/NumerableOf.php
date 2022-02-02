296ca7959327bc613dbbe9c80bae877e8d425a07
maximtrunnikov <m.trunnikov@mail.ru>
2022-02-03 01:52:02 +0300

    Added Text support to NumerableOf

--- src/Numerable/NumerableOf.php
+++ src/Numerable/NumerableOf.php
@@ -3,6 +3,7 @@
 namespace Maxonfjvipon\Elegant_Elephant\Numerable;
 
 use Maxonfjvipon\Elegant_Elephant\Numerable;
+use Maxonfjvipon\Elegant_Elephant\Text;
 use Maxonfjvipon\OverloadedElephant\Overloadable;
 
 /**
@@ -14,24 +15,24 @@ final class NumerableOf implements Numerable
     use Overloadable;
 
     /**
-     * @var float|int|string $origin
+     * @var float|int|Text|string $origin
      */
-    private float|int|string $origin;
+    private float|int|string|Text $origin;
 
     /**
-     * @param float|int|string $num
+     * @param float|int|Text|string $num
      * @return NumerableOf
      */
-    public static function new(float|int|string $num): NumerableOf
+    public static function new(float|int|Text|string $num): NumerableOf
     {
         return new self($num);
     }
 
     /**
      * Ctor.
-     * @param float|int|string $num
+     * @param float|int|Text|string $num
      */
-    public function __construct(float|int|string $num)
+    public function __construct(float|int|Text|string $num)
     {
         $this->origin = $num;
     }
@@ -44,7 +45,8 @@ final class NumerableOf implements Numerable
         return self::overload([$this->origin], [[
             'double',
             'integer',
-            'string' => fn(string $num) => $num * 1
+            'string'    => fn(string $num) => $num * 1,
+            Text::class => fn(Text $text) => $text->asString() * 1
         ]])[0];
     }
 }
