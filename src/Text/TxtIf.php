8eecdba029b460571d1c15bb50c1aa931bbf6990
maximtrunnikov <m.trunnikov@mail.ru>
2022-05-29 00:16:38 +0300

    Added TxtIf

---
+++ src/Text/TxtIf.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Maxonfjvipon\Elegant_Elephant\Text;
+
+use Maxonfjvipon\Elegant_Elephant\Logical;
+use Maxonfjvipon\Elegant_Elephant\Text;
+
+class TxtIf implements Text
+{
+
+    /**
+     * @var bool|Logical $condition
+     */
+    private bool|Logical $condition;
+
+    /**
+     * @var string|Text $arr
+     */
+    private string|Text $text;
+
+    /**
+     * Ctor wrap.
+     * @param bool|Logical $cond
+     * @param string|Text $text
+     * @return TxtIf
+     */
+    public static function new(bool|Logical $cond, string|Text $text): TxtIf
+    {
+        return new self($cond, $text);
+    }
+
+    /**
+     * Ctor.
+     * @param bool|Logical $cond
+     * @param string|Text $text
+     */
+    public function __construct(bool|Logical $cond, string|Text $text)
+    {
+        $this->condition = $cond;
+        $this->text = $text;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function asString(): string
+    {
+        return (new TxtTernary(
+            $this->condition,
+            $this->text,
+            new TxtBlank()
+        ))->asString();
+    }
+}
