8152b692b36e66f88b359611d2b3c2c5c098aa9a
maxonfjvipon <mtrunnikov@gmail.com>
2022-06-19 23:42:57 +0300

    Introduced ArrFlatten

---
+++ src/Arrayable/ArrFlatten.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Maxonfjvipon\Elegant_Elephant\Arrayable;
+
+use Exception;
+use Maxonfjvipon\Elegant_Elephant\Arrayable;
+
+/**
+ * Flatten arrayable
+ */
+final class ArrFlatten extends ArrayableIterable
+{
+    use ArrayableOverloaded;
+
+    /**
+     * @var array|Arrayable $arr
+     */
+    private array|Arrayable $arr;
+
+    /**
+     * @var int $deep
+     */
+    private int $deep;
+
+    /**
+     * Ctor wrap.
+     * @param array|Arrayable $arr
+     * @param int $deep
+     * @return ArrFlatten
+     */
+    public static function new(array|Arrayable $arr, int $deep = 1): self
+    {
+        return new self($arr, $deep);
+    }
+
+    /**
+     * Ctor.
+     * @param array|Arrayable $arr
+     * @param int $deep
+     */
+    public function __construct(array|Arrayable $arr, int $deep = 1)
+    {
+        $this->arr = $arr;
+        $this->deep = $deep;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function asArray(): array
+    {
+        return $this->flat($this->firstArrayableOverloaded($this->arr), []);
+    }
+
+    /**
+     * @param array $arr
+     * @param $new
+     * @param int $deep
+     * @return array
+     * @throws Exception
+     */
+    private function flat(array $arr, $new, int $deep = 0): array
+    {
+        foreach ($arr as $item) {
+            if ($this->deep !== $deep && (is_array($item) || $item instanceof Arrayable)) {
+                $new = $this->flat($this->firstArrayableOverloaded($item), $new, $deep + 1);
+            } else {
+                $new[] = $item;
+            }
+        }
+        return $new;
+    }
+}
---
+++ tests/Arrayable/ArrFlattenTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Maxonfjvipon\Elegant_Elephant\Tests\Arrayable;
+
+use Maxonfjvipon\Elegant_Elephant\Arrayable\ArrayableOf;
+use Maxonfjvipon\Elegant_Elephant\Arrayable\ArrFlatten;
+use PHPUnit\Framework\TestCase;
+
+final class ArrFlattenTest extends TestCase
+{
+    /**
+     * @throws \Exception
+     */
+    public function testAsArray()
+    {
+        $this->assertEquals(
+            [1, 2, 3, 4, 5],
+            ArrFlatten::new([1, [2, 3], 4, [5]])->asArray()
+        );
+        $this->assertEquals(
+            [1, 2, 3, 4, 5, 6],
+            ArrFlatten::new(new ArrayableOf([1, new ArrayableOf([2, 3], false), [[4]],
+                new ArrayableOf(new ArrayableOf([5, 6], false), false)]),
+                2
+            )->asArray()
+        );
+    }
+}
