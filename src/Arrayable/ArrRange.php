3ed17832c6b97e5dce4ef59d726ef6cc36cf54b0
maxonfjvipon <mtrunnikov@gmail.com>
2022-06-23 01:27:50 +0300

    ArrRange fixed

--- src/Arrayable/ArrRange.php
+++ src/Arrayable/ArrRange.php
@@ -2,32 +2,39 @@
 
 namespace Maxonfjvipon\Elegant_Elephant\Arrayable;
 
+use Exception;
+use Maxonfjvipon\Elegant_Elephant\Any;
 use Maxonfjvipon\Elegant_Elephant\Arrayable;
+use Maxonfjvipon\Elegant_Elephant\Numerable;
+use Maxonfjvipon\Elegant_Elephant\Numerable\NumerableOverloaded;
 
 /**
  * Arr range.
  */
 final class ArrRange implements Arrayable
 {
+    use NumerableOverloaded;
+
     /**
      * Ctor.
-     * @param int|float $from
-     * @param int|float $to
-     * @param int|float $step
+     * @param int|float|Numerable|Any $from
+     * @param int|float|Numerable|Any $to
+     * @param int|float|Numerable|Any $step
      */
     public function __construct(
-        private int|float $from,
-        private int|float $to,
-        private int|float $step = 1
+        private int|float|Numerable|Any $from,
+        private int|float|Numerable|Any $to,
+        private int|float|Numerable|Any $step = 1
     )
     {
     }
 
     /**
      * @return array
+     * @throws Exception
      */
     public function asArray(): array
     {
-        return range($this->from, $this->to, $this->step);
+        return range(...$this->numerableOverloaded($this->from, $this->to, $this->step));
     }
 }
