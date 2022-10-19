00a88996a85875a014bb16b361c71b990c91c145
maximtrunnikov <m.trunnikov@mail.ru>
2022-10-20 00:09:15 +0300

    comments

--- src/Text/TxtUpper.php
+++ src/Text/TxtUpper.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Maxonfjvipon\Elegant_Elephant\Text;
 
-use Exception;
 use Maxonfjvipon\Elegant_Elephant\Scalar\CastMixed;
 use Maxonfjvipon\Elegant_Elephant\Text;
 
